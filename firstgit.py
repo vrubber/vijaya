50b537baa299fc34ed0af7470d605a2440258161
vrubber <116381590+vrubber@users.noreply.github.com>
2022-10-21 23:38:00 +0530

    Create firstgit.py

---
+++ firstgit.py
@@ -0,0 +1,42 @@
+# def freq(x):
+#     d={ }
+#     for i in x:
+#         if i in d:
+#             d[i]+=1
+#         else:
+#             d[i]=1
+#     for i in d:
+#      print(i,":",d[i])
+
+# x=["abc","efg","aadf","gfed","asdf"]
+# z='a'
+# y=[ ]
+# for i in x:
+#     if z not in i:
+#         y.append(i)
+# print(y)
+
+
+#even placed chars at one output and odd placed chars another output
+# x="rubber"
+# y=" "
+# z=" "
+# for i in range (len(x)):
+#     if(i%2==0):
+#         y=y+x[i]
+#     else:
+#         z=z+x[i]
+# print(y)
+# print(z)
+
+# vowels and consonents
+# x=['a','e','c','d','o']
+# y = [ ]
+# z = [ ]
+# for i in x:
+#     if i in ['a','e','i','o','u']:
+#         y.append(i)
+#     else:
+#         z.append(i)
+# print(y)
+# print(z)
